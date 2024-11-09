21f57605f4f65e3135a52c8031165afb7dc92d87
Neil Aldea <njaldea@gmail.com>
2024-11-09 08:30:18 +0800

    [xit] sandbox_test Base install implementation

--- sandbox/test/main.cpp
+++ sandbox/test/main.cpp
@@ -9,6 +9,7 @@
 #include <iostream>
 #include <string_view>
 #include <tuple>
+#include <type_traits>
 #include <unordered_map>
 #include <utility>
 
@@ -80,58 +81,77 @@ struct IFrameInfo
     virtual ~IFrameInfo() = default;
 };
 
-template <typename T>
-struct UniqueFrameInfo: IFrameInfo
+namespace input
 {
-    using type = T;
-    nil::gate::edges::Mutable<T>* input = nullptr;
 
-    nil::gate::edges::Compatible<T> get_input(std::string_view /* tag */)
+    template <typename T>
+    struct FrameInfo: IFrameInfo
     {
-        return input;
-    }
+        using type = T;
 
-    void add_tag(std::string_view /* tag */, nil::gate::Core& /* gate */)
-    {
-    }
-};
+        FrameInfo() = default;
+        FrameInfo(FrameInfo&&) = delete;
+        FrameInfo(const FrameInfo&) = delete;
+        FrameInfo& operator=(FrameInfo&&) = delete;
+        FrameInfo& operator=(const FrameInfo&) = delete;
+        ~FrameInfo() override = default;
 
-template <typename T>
-struct TaggedFrameInfo: IFrameInfo
-{
-    using type = T;
-
-    struct Entry
-    {
-        std::optional<T> data;
-        nil::gate::edges::Mutable<T>* input = nullptr;
+        virtual nil::gate::edges::Compatible<T> get_input(std::string_view tag) = 0;
+        virtual void add_tag(std::string_view tag, nil::gate::Core& gate) = 0;
     };
 
-    transparent::hash_map<Entry> info;
-
-    nil::gate::edges::Compatible<T> get_input(std::string_view tag)
+    template <typename T>
+    struct UniqueFrameInfo: FrameInfo<T>
     {
-        if (const auto it = info.find(tag); it != info.end())
+        nil::gate::edges::Mutable<T>* input = nullptr;
+
+        nil::gate::edges::Compatible<T> get_input(std::string_view /* tag */) override
         {
-            return it->second.input;
+            return input;
         }
-        return nullptr;
-    }
 
-    void add_tag(std::string_view tag, nil::gate::Core& gate)
+        void add_tag(std::string_view /* tag */, nil::gate::Core& /* gate */) override
+        {
+        }
+    };
+
+    template <typename T>
+    struct TaggedFrameInfo: FrameInfo<T>
     {
-        auto* edge = gate.edge<T>();
-        info.emplace(tag, typename TaggedFrameInfo<T>::Entry{std::nullopt, edge});
-    }
-};
+        struct Entry
+        {
+            std::optional<T> data;
+            nil::gate::edges::Mutable<T>* input = nullptr;
+        };
 
-template <typename T>
-struct OutputFrameInfo: IFrameInfo
+        transparent::hash_map<Entry> info;
+
+        nil::gate::edges::Compatible<T> get_input(std::string_view tag) override
+        {
+            if (const auto it = info.find(tag); it != info.end())
+            {
+                return it->second.input;
+            }
+            return nullptr;
+        }
+
+        void add_tag(std::string_view tag, nil::gate::Core& gate) override
+        {
+            info.emplace(tag, typename TaggedFrameInfo<T>::Entry{std::nullopt, gate.edge<T>()});
+        }
+    };
+}
+
+namespace output
 {
-    using type = T;
-    transparent::hash_map<nil::gate::edges::Mutable<RerunTag>*> rerun;
-    nil::xit::tagged::Value<T>* output = nullptr;
-};
+    template <typename T>
+    struct FrameInfo: IFrameInfo
+    {
+        using type = T;
+        transparent::hash_map<nil::gate::edges::Mutable<RerunTag>*> rerun;
+        nil::xit::tagged::Value<T>* output = nullptr;
+    };
+}
 
 struct App
 {
@@ -146,47 +166,65 @@ struct App
     App& operator=(App&&) = delete;
     App& operator=(const App&) = delete;
 
-    template <typename Callable, typename... I, typename... O>
+    template <typename Callable, typename... Inputs, typename... Outputs>
     void add_node(
         std::string_view tag,
         Callable callable,
-        std::tuple<I*...> inputs,
-        std::tuple<O*...> outputs
+        std::tuple<Inputs...> inputs,
+        std::tuple<Outputs...> outputs
     )
     {
         apply([tag, this](auto*... input) { (input->add_tag(tag, this->gate), ...); }, inputs);
-        auto gate_outputs = gate.node(
-            std::move(callable),
-            std::apply([tag](auto*... i) { return std::make_tuple(i->get_input(tag)...); }, inputs)
-        );
-        for_each(
-            [&](auto* output, auto* gate_output)
-            {
-                using o_t = typename std::decay_t<std::remove_pointer_t<decltype(output)>>::type;
-                auto& [key, rerun] = *output->rerun.emplace(tag, this->gate.edge(RerunTag())).first;
-                gate.node(
-                    [output, t = &key](RerunTag, const o_t& output_data)
-                    {
-                        std::cout << "out (test) " << *t << std::endl;
-                        post(*t, *output->output, output_data);
-                    },
-                    {rerun, gate_output}
-                );
-            },
-            outputs,
-            gate_outputs,
-            std::make_index_sequence<sizeof...(O)>()
-        );
+        if constexpr (sizeof...(Outputs) == 0)
+        {
+            gate.node(
+                std::move(callable),
+                std::apply(
+                    [tag](auto*... i) { return std::make_tuple(i->get_input(tag)...); },
+                    inputs
+                )
+            );
+        }
+        else
+        {
+            auto gate_outputs = gate.node(
+                std::move(callable),
+                std::apply(
+                    [tag](auto*... i) { return std::make_tuple(i->get_input(tag)...); },
+                    inputs
+                )
+            );
+            for_each(
+                [&](auto* output, auto* gate_output)
+                {
+                    using o_t =
+                        typename std::decay_t<std::remove_pointer_t<decltype(output)>>::type;
+                    auto& [key, rerun]
+                        = *output->rerun.emplace(tag, this->gate.edge(RerunTag())).first;
+                    gate.node(
+                        [output, t = &key](RerunTag, const o_t& output_data)
+                        {
+                            std::cout << "out (test) " << *t << std::endl;
+                            post(*t, *output->output, output_data);
+                        },
+                        {rerun, gate_output}
+                    );
+                },
+                outputs,
+                gate_outputs,
+                std::make_index_sequence<sizeof...(Outputs)>()
+            );
+        }
     }
 
     template <typename T>
-    TaggedFrameInfo<T>* add_tagged_input(
+    input::TaggedFrameInfo<T>* add_tagged_input(
         std::string id,
         std::filesystem::path path,
         std::function<T(std::string_view)> initializer
     )
     {
-        auto* s = make_frame<TaggedFrameInfo<T>>(id, input_frames);
+        auto* s = make_frame<input::TaggedFrameInfo<T>>(id, input_frames);
         auto& frame = add_tagged_frame(xit, std::move(id), std::move(path));
         add_value(
             frame,
@@ -221,9 +259,13 @@ struct App
     }
 
     template <typename T>
-    UniqueFrameInfo<T>* add_unique_input(std::string id, std::filesystem::path path, T init_data)
+    input::UniqueFrameInfo<T>* add_unique_input(
+        std::string id,
+        std::filesystem::path path,
+        T init_data
+    )
     {
-        auto* s = make_frame<UniqueFrameInfo<T>>(id, input_frames);
+        auto* s = make_frame<input::UniqueFrameInfo<T>>(id, input_frames);
         s->input = gate.edge(init_data);
         auto& frame = add_unique_frame(xit, std::move(id), std::move(path));
         add_value(
@@ -240,9 +282,9 @@ struct App
     }
 
     template <typename T>
-    OutputFrameInfo<T>* add_output(std::string id, std::filesystem::path path)
+    output::FrameInfo<T>* add_output(std::string id, std::filesystem::path path)
     {
-        auto* s = make_frame<OutputFrameInfo<T>>(id, output_frames);
+        auto* s = make_frame<output::FrameInfo<T>>(id, output_frames);
         auto& frame = add_tagged_frame(
             xit,
             std::move(id),
@@ -261,11 +303,11 @@ struct App
     }
 
     template <typename T>
-    T* make_frame(std::string_view tag, auto& frames)
+    T* make_frame(std::string_view id, auto& frames)
     {
         auto t = std::make_unique<T>();
         auto p = t.get();
-        frames.emplace(tag, std::move(t));
+        frames.emplace(id, std::move(t));
         return p;
     }
 
@@ -276,24 +318,118 @@ struct App
     std::unordered_map<std::string, std::unique_ptr<IFrameInfo>> output_frames;
 };
 
-template <typename T>
-using input_t = std::tuple<std::string, T>;
-template <typename T>
-using output_t = std::string;
-
-struct Callable
+template <typename... T>
+struct type
 {
-    std::string tag;
-    std::uint32_t index;
-
-    nlohmann::json operator()(const nlohmann::json& input_data, std::int64_t value) const
-    {
-        std::cout << "run (test) " << tag << std::endl;
-        auto result = input_data;
-        result[0]["y"][index] = input_data[0]["y"][index].get<std::int64_t>() * value;
-        return result;
-    }
 };
+
+template <size_t N>
+struct StringLiteral
+{
+    // NOLINTNEXTLINE
+    constexpr StringLiteral(const char (&str)[N])
+    {
+        // NOLINTNEXTLINE
+        std::copy_n(str, N, value);
+    }
+
+    // NOLINTNEXTLINE
+    char value[N];
+};
+
+template <typename T, StringLiteral S>
+struct Frame
+{
+    using type = T;
+};
+
+template <typename... T>
+struct Inputs
+{
+    std::tuple<const T*...> data;
+};
+
+template <typename... T>
+struct std::tuple_size<Inputs<T...>>: std::integral_constant<std::size_t, sizeof...(T)>
+{
+};
+
+template <std::size_t I, typename... T>
+const auto& get(const Inputs<T...>& o)
+{
+    return *std::get<I>(o.data);
+}
+
+template <std::size_t I, typename... T>
+struct std::tuple_element<I, Inputs<T...>>
+{
+    using type = decltype(*get<I>(std::declval<Inputs<T...>>().data));
+};
+
+template <typename P, typename I, typename O>
+struct Base;
+
+template <typename P, typename... T, StringLiteral... I, typename... U, StringLiteral... O>
+struct Base<P, std::tuple<Frame<T, I>...>, std::tuple<Frame<U, O>...>>
+{
+    virtual ~Base() noexcept = default;
+    Base() = default;
+    Base(Base&&) = delete;
+    Base(const Base&) = delete;
+    Base& operator=(Base&&) = delete;
+    Base& operator=(const Base&) = delete;
+
+    static void install(App& a, std::string_view tag)
+    {
+        a.add_node(
+            tag,
+            [](const T&... args) -> return_t
+            {
+                P p;
+                return p.run(argument_t{{std::addressof(args)...}});
+            },
+            std::make_tuple( // NOLINTNEXTLINE
+                static_cast<input::FrameInfo<T>*>(a.input_frames.at(std::string(I.value)).get())...
+            ),
+            std::make_tuple( // NOLINTNEXTLINE
+                static_cast<output::FrameInfo<U>*>(a.output_frames.at(std::string(O.value)).get()
+                )...
+            )
+        );
+    }
+
+    using return_t = std::conditional_t<sizeof...(U) == 0, void, std::tuple<U...>>;
+    using argument_t = Inputs<T...>;
+};
+
+struct Derived
+    : Base<
+          Derived,
+          std::tuple<Frame<nlohmann::json, "editor_frame">, Frame<std::int64_t, "slider_frame">>,
+          std::tuple<Frame<nlohmann::json, "view_frame">> //
+          >
+{
+    Derived() = default;
+    return_t run(argument_t args);
+};
+
+// TODO: optionality of returning? pass as an output argument?
+Derived::return_t Derived::run(Derived::argument_t args) // NOLINT
+{
+    const auto& [input_data, value] = args;
+    auto tag = std::string(input_data[0]["x"][2]);
+    std::cout << "run (test) " << tag << std::endl;
+    auto result = input_data;
+    if (tag == "a")
+    {
+        result[0]["y"][0] = input_data[0]["y"][0].get<std::int64_t>() * value;
+    }
+    else if (tag == "b")
+    {
+        result[0]["y"][1] = input_data[0]["y"][1].get<std::int64_t>() * value;
+    }
+    return {result};
+}
 
 int main()
 {
@@ -315,11 +451,13 @@ int main()
     add_value(main_frame, "pane", nlohmann::json::parse(R"(["slider_frame", "editor_frame"])"));
 
     app.gate.set_runner<nil::gate::runners::NonBlocking>();
-    auto* editor = app.add_tagged_input<nlohmann::json>(
+    app.add_tagged_input<nlohmann::json>(
         "editor_frame",
         "gui/EditorFrame.svelte",
         [](std::string_view tag)
         {
+            // TODO: move this to the frame itself
+            // add trait to be able to read from and write to file
             return nlohmann::json::array({nlohmann::json::object(
                 {{"x", nlohmann::json::array({"giraffes", "orangutans", tag})},
                  {"y", nlohmann::json::array({20, 14, 23})},
@@ -327,11 +465,12 @@ int main()
             )});
         }
     );
-    auto* slider = app.add_unique_input<std::int64_t>("slider_frame", "gui/Slider.svelte", 0L);
-    auto* view = app.add_output<nlohmann::json>("view_frame", "gui/ViewFrame.svelte");
+    app.add_unique_input<std::int64_t>("slider_frame", "gui/Slider.svelte", 0L);
+    app.add_output<nlohmann::json>("view_frame", "gui/ViewFrame.svelte");
 
-    app.add_node("a", Callable("a", 0), std::make_tuple(editor, slider), std::make_tuple(view));
-    app.add_node("b", Callable("b", 1), std::make_tuple(editor, slider), std::make_tuple(view));
+    // TODO: store this somewhere to be called during registration
+    Derived::install(app, "a");
+    Derived::install(app, "b");
 
     // TODO:
     //  - test api for registering the input/output frames
